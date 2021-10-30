268864d5544ff8ade1fe83dfc295c78ffc0e9f5b
RyanDuong1 <82671859+RyanDuong1@users.noreply.github.com>
2021-10-30 18:53:26 -0400

    Update ImageUtil.java

--- src/util/ImageUtil.java
+++ src/util/ImageUtil.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
 
 
 /**
- * This class contains utility methods to read a PPM image from file and simply print its contents. Feel free to change this method 
+ * This class contains utility methods to read a PPM image from file and simply print its contents. Feel free to change this method
  *  as required.
  */
 public class ImageUtil {
@@ -16,65 +16,73 @@ public class ImageUtil {
    *
    * @param filename the path of the file.
    */
-  public static void readPPM(String filename) {
+  public static Pixel[][] readPPM(String filename) {
     Scanner sc;
-    
+    Pixel[][] pixelMatrix = null;
+
     try {
-        sc = new Scanner(new FileInputStream(filename));
-    }
-    catch (FileNotFoundException e) {
-        System.out.println("File "+filename+ " not found!");
-        return;
-    }
-    StringBuilder builder = new StringBuilder();
-    //read the file line by line, and populate a string. This will throw away any comment lines
-    while (sc.hasNextLine()) {
+      sc = new Scanner(new FileInputStream(filename));
+
+      StringBuilder builder = new StringBuilder();
+      //read the file line by line, and populate a string. This will throw away any comment lines
+      while (sc.hasNextLine()) {
         String s = sc.nextLine();
-        if (s.charAt(0)!='#') {
-            builder.append(s+System.lineSeparator());
+        if (s.charAt(0) != '#') {
+          builder.append(s + System.lineSeparator());
         }
-    }
-    
-    //now set up the scanner to read from the string we just built
-    sc = new Scanner(builder.toString());
+      }
 
-    String token; 
+      //now set up the scanner to read from the string we just built
+      sc = new Scanner(builder.toString());
 
-    token = sc.next();
-    if (!token.equals("P3")) {
+      String token;
+
+      token = sc.next();
+      if (!token.equals("P3")) {
         System.out.println("Invalid PPM file: plain RAW file should begin with P3");
-    }
-    int width = sc.nextInt();
-    System.out.println("Width of image: "+width);
-    int height = sc.nextInt();
-    System.out.println("Height of image: "+height);
-    int maxValue = sc.nextInt();
-    System.out.println("Maximum value of a color in this file (usually 255): "+maxValue);
+      }
+      int width = sc.nextInt();
+      //System.out.println("Width of image: " + width);
+      int height = sc.nextInt();
+      //System.out.println("Height of image: " + height);
+      int maxValue = sc.nextInt();
 
-    Pixel[][] matrix = new Pixel[width][height];
+      pixelMatrix = new Pixel[height][width];
 
-    for (int i=0;i<height;i++) {
-        for (int j=0;j<width;j++) {
-            int r = sc.nextInt();
-            int g = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println("Color of pixel ("+j+","+i+"): "+ r+","+g+","+b);
+      //System.out.println("Maximum value of a color in this file (usually 255): "+maxValue);
+      for (int j = 0; j < height; j++) {
+      for (int i = 0; i < width; i++) {
+
+          int r = sc.nextInt();
+          int g = sc.nextInt();
+          int b = sc.nextInt();
+          pixelMatrix[j][i] = new Pixel(r, g, b);
+
+          //System.out.println("Color of pixel ("+j+","+i+"): "+ r+","+g+","+b);
         }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File "+filename+ " not found!");
+      System.exit(0);
     }
+    return pixelMatrix;
   }
+
+
 
   //demo main
   public static void main(String []args) {
-      String filename;
-      
-      if (args.length>0) {
-          filename = args[0];
-      }
-      else {
-          filename = "Koala.ppm";
-      }
-      
-      ImageUtil.readPPM(filename);
+    String filename;
+
+    if (args.length>0) {
+      filename = args[0];
+    }
+    else {
+      filename = "/Users/ryankii/Desktop/OOD/Hw4/src/Koala.ppm";
+    }
+
+    ImageUtil.readPPM(filename);
   }
 }
+
 
