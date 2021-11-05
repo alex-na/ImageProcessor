7ee5f12427feba51266761343faea12e2a2842e7
Alex <alexnaish19@gmail.com>
2021-11-04 23:04:07 -0400

    added transformation command

---
+++ src/controller/commands/Transformation.java
@@ -0,0 +1,45 @@
+package controller.commands;
+
+import model.ImageProcessingModel;
+import view.ImageProcessingView;
+
+public class Transformation implements ImageProcessingCommand {
+
+  private String type;
+  private String imageName;
+  private String desiredName;
+
+  /**
+   * Constructs a Transformation object.
+   *
+   * @param type        filter type
+   * @param imageName   name of the image to perform filter command on
+   * @param desiredName desired name of the image
+   * @throws IllegalArgumentException if any inputs are null
+   */
+  public Transformation(String type, String imageName, String desiredName)
+      throws IllegalArgumentException {
+    if (type == null || imageName == null || desiredName == null) {
+      throw new IllegalArgumentException("Inputs may not be null.");
+    }
+    this.type = type;
+    this.imageName = imageName;
+    this.desiredName = desiredName;
+  }
+
+  @Override
+  public void apply(ImageProcessingModel model, ImageProcessingView view)
+      throws IllegalArgumentException {
+
+    switch (this.type) {
+      case "greyscale" :
+        // model.transformImage("greyscale", imageName, desiredName);
+        break;
+      case "sepia" :
+        // model.transformImage("greyscale", imageName, desiredName);
+        break;
+      default: throw new IllegalArgumentException("Invalid command.");
+    }
+
+  }
+}
